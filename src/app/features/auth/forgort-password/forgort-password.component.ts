e06d35b9a0ff26107c727756b4539314016b8754
KawanC <kcosta245@gmail.com>
2025-04-19 19:42:00 -0300

    Criando primeiros arquivos

---
+++ src/app/features/auth/forgort-password/forgort-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgort-password',
+  imports: [],
+  templateUrl: './forgort-password.component.html',
+  styleUrl: './forgort-password.component.scss'
+})
+export class ForgortPasswordComponent {
+
+}
